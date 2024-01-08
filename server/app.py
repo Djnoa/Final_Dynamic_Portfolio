f1b9b6626786983cfea62b5d2a28eac7adc2344d
Djnoa <dominicnoakes@icloud.com>
2024-01-08 11:28:36 +0100

    commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, send_from_directory
 import sqlite3
 from flask_cors import CORS  
-import os
+# import os
  
 app = Flask(__name__, static_folder='../client/build', static_url_path='/')
 CORS(app) 
